6a4bd34e9424f775b6655145a59703d50f684356
Susana vik <susana.m.vik@gmail.com>
2021-02-15 03:08:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
-# Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +127,74 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(player_name)
+  game_hash.each do |location, keys|
+    keys[:players].each do |player|
+      if player_name == player[:player_name]
+      return player[:points]
+      end
+    end
+  end
+end
+
+def shoe_size(player_name)
+  game_hash.each do |location, keys|
+    keys[:players].each do |player|
+      if player_name == player[:player_name]
+      return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team_name)
+  game_hash.each do |location, keys|
+    if team_name == keys[:team_name]
+      return keys[:colors]
+    end
+  end
+end
+
+def team_names
+  game_hash.map do |home_away, keys|
+    keys[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  game_hash.each do |location, keys|
+    if team_name == keys[:team_name]
+      return keys[:players].map { |player| player[:number] }
+    end
+  end
+end
+
+def player_stats(players_name)
+  game_hash.each do |location, keys|
+    keys[:players].each do |player|
+      if player_name == player[:player_name]
+      return player[:points]
+      return player[:rebounds]
+      return player[:assists]
+      return player[:steals]
+      return player[:blocks]
+      return player[:slam_dunks]
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  biggest = 0
+  game_hash.each do |location, keys|
+    keys[:players].each do |player|
+      size = player[:shoe]
+        if size > biggest
+          biggest = size
+          player[:rebounds]
+          
+        end
+      end
+    end
+    rebounds
+end
